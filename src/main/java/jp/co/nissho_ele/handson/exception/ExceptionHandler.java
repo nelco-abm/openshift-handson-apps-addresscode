63013446bcb855fd9387846e023a6ed0467a39e6
soharaki <soharaki@nissho-ele.co.jp>
2020-08-28 05:36:56 +0000

    Add Exception Log

--- src/main/java/jp/co/nissho_ele/handson/exception/ExceptionHandler.java
+++ src/main/java/jp/co/nissho_ele/handson/exception/ExceptionHandler.java
@@ -1,30 +1,27 @@
 package jp.co.nissho_ele.handson.exception;
 
+import javax.json.Json;
 import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.ext.ExceptionMapper;
 import javax.ws.rs.ext.Provider;
 
-import javax.json.Json;
+import org.slf4j.Logger;
 
-//@Provider
-public class ExceptionHandler
-    implements ExceptionMapper<Exception> {
+@Provider
+public class ExceptionHandler implements ExceptionMapper<Exception> {
+
+    private static final Logger LOG = Logger.getLogger(ExceptionHandler.class);
 
     @Override
     public Response toResponse(Exception exception) {
         int code = 500;
         if (exception instanceof WebApplicationException) {
-            code = ((WebApplicationException) exception)
-                .getResponse().getStatus();
+            code = ((WebApplicationException) exception).getResponse().getStatus();
         }
+        LOG.error("exception happened");
         return Response.status(code)
-            .entity(
-                Json.createObjectBuilder()
-                .add("error", exception.getMessage())
-                .add("code", code)
-                .build()
-            )
-            .build();
-        }
+                .entity(Json.createObjectBuilder().add("error", exception.getMessage()).add("code", code).build())
+                .build();
     }
+}
